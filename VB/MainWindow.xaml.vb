6bc51212c91b98ada5cc122e2107c608babdaa34
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+
+Namespace WpfApplication2
+
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+
+			carSeries.DataSource = New CarsDBDataSetTableAdapters.CarsTableAdapter().GetData()
+		End Sub
+	End Class
+End Namespace
